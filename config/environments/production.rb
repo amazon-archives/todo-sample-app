6e5ee4241f6a3618369b4ad0b026fc8eaa602c2f
Alex Wood <awood45@gmail.com>
2014-08-20 16:21:59 -0700

    Set Amazon CloudFront as Asset Server
    Expects an environment variable CLOUDFRONT_ENDPOINT to be set, which
    will contain the endpoint URL of the CloudFront distribution which is
    serving the app's static assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = ENV['CLOUDFRONT_ENDPOINT']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
